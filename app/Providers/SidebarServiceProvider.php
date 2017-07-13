b83c76053c433b3c0f76f4bfd77452b8a625ec69
skylerfenn <skylerfenn@users.noreply.github.com>
2017-07-12 23:42:59 -0600

    Fix DocBlock

--- app/Providers/SidebarServiceProvider.php
+++ app/Providers/SidebarServiceProvider.php
@@ -9,7 +9,7 @@ class SidebarServiceProvider extends BaseSidebarServiceProvider
     /**
      * An array of sidebars to be loaded as Wordpress sidebars.
      *
-     * @see LaraPress\Sidebars\SidebarServiceProvider for proper structure
+     * @see \LaraPress\Sidebars\SidebarServiceProvider for proper structure
      * @var array
      */
     protected $sidebars = [
