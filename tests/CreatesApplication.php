d23419ad94a5bef3a1c9c1826e4cfe907443041c
Skyler Fenn <skylerfenn@msn.com>
2018-01-21 11:46:59 -0700

    Code style tweak

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -16,7 +16,7 @@ trait CreatesApplication
     {
         require_once __DIR__ . '/../public/wp-config.php';
 
-        $app = require __DIR__.'/../bootstrap/app.php';
+        $app = require __DIR__ . '/../bootstrap/app.php';
 
         $app->make(Kernel::class)->bootstrap();
 
